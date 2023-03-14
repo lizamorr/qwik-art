287844b9f294e4209b6d905ce982bd73346c7657
Liza Morrison <lizammorrison@gmail.com>
2023-03-14 10:43:21 +0100

    upgrade qwik to 21

---
+++ adapters/vercel-edge/vite.config.ts
@@ -0,0 +1,20 @@
+import baseConfig from "../../vite.config";
+import { extendConfig } from "@builder.io/qwik-city/vite";
+import { vercelEdgeAdapter } from "@builder.io/qwik-city/adapters/vercel-edge/vite";
+
+export default extendConfig(baseConfig, () => {
+  return {
+    build: {
+      ssr: true,
+      rollupOptions: {
+        input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"],
+      },
+      outDir: ".vercel/output/functions/_qwik-city.func",
+    },
+    plugins: [
+      vercelEdgeAdapter({
+        staticGenerate: true,
+      }),
+    ],
+  };
+});
