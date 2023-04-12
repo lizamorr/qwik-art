3c42b7f2a97454c066309f4c0960ea536c55057a
Liza Morrison <lizammorrison@gmail.com>
2023-04-12 15:40:44 +0200

    add github to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,8 +2,11 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <footer class="bg-white fixed bottom-0 left-0 p-2 shadow z-10 w-full flex justify-center tracking-wider">
-      {`© Liza Morrison ${new Date().getFullYear()}`}
+    <footer class="cursor-pointer bg-white fixed bottom-0 left-0 p-2 shadow z-10 w-full flex justify-center tracking-wider">
+      <a
+        href="https://github.com/lizamorr/artwork-website"
+        target="_blank"
+      >{`© Liza Morrison ${new Date().getFullYear()}`}</a>
     </footer>
   );
 });
