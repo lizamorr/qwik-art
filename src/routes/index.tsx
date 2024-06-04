f908538f32c77ffe5407ea8316b109390f06968f
Liza Morrison <lizammorrison@gmail.com>
2024-06-04 10:23:41 +0200

    remove labrinth poster

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { component$ } from "@builder.io/qwik";
 import labrinth from "./labrinth.mp4";
-import labrinthPoster from "./lab-thumbnail.webp";
 import title from "./name.gif";
 
 export default component$(() => {
@@ -15,7 +14,6 @@ export default component$(() => {
         loop
         class="object-cover w-auto h-full fixed top-0"
         id="labrinth"
-        poster={labrinthPoster}
       />
       <img
         src={title}
