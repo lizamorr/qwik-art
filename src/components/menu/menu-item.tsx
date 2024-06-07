e129f240639dd9e63f35e089d5d7db7e67229cc6
Liza Morrison <lizammorrison@gmail.com>
2024-06-07 20:13:27 +0200

    use qwik link

--- src/components/menu/menu-item.tsx
+++ src/components/menu/menu-item.tsx
@@ -1,6 +1,5 @@
+import { Link, useLocation } from "@builder.io/qwik-city";
 import { component$, useSignal } from "@builder.io/qwik";
-
-import { useLocation } from "@builder.io/qwik-city";
 
 export default component$((props: { name: string }) => {
   const loc = useLocation();
@@ -14,14 +13,14 @@ export default component$((props: { name: string }) => {
 
   return (
     <div class="flex m-0 p-0">
-      <a
+      <Link
         class={`text-4xl md:text-2xl hover:underline underline-offset-8 tracking-wider ${
           isPageSelected.value ? "underline underline-offset-8" : "no-underline"
         }`}
         href={navigateTo.value}
       >
         {props.name}
-      </a>
+      </Link>
     </div>
   );
 });
